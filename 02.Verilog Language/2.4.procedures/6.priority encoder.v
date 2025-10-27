66d73ee1211ca851d292b69a9e8e4b389653200f
Gokul S <sarangokul86@gmail.com>
2025-10-27 18:32:41 +0530

    Update 6.priority encoder.v

--- 02.Verilog Language/2.4.procedures/6.priority encoder.v
+++ 02.Verilog Language/2.4.procedures/6.priority encoder.v
@@ -1,25 +1,16 @@
 module top_module (
     input [3:0] in,
     output reg [1:0] pos  );
-    
-    always@(*) begin
-        case(in)
-            4'b0000 : pos = 1'b0;
-            4'b0001 : pos = 1'b0;
-            4'b0010 : pos = 2'b01;
-            4'b0011 : pos = 2'b0;
-            4'b0100 : pos = 2'b10;
-            4'b0101 : pos = 2'b0;
-            4'b0110 : pos = 2'b01;
-            4'b0111 : pos = 2'b0;
-            4'b1000 : pos = 2'b11;
-            4'b1001 : pos = 2'b0;
-            4'b1010 : pos = 2'b01;
-            4'b1011 : pos = 2'b0;
-            4'b1100 : pos = 2'b10;
-            4'b1101 : pos = 2'b0;
-            4'b1110 : pos = 2'b01;
-            4'b1111 : pos = 2'b0;
-        endcase
+    always@(*)begin
+        if(in[0])
+            pos=2'b00;
+        else if(in[1])
+            pos=2'b01;
+        else if(in[2])
+            pos=2'b10;
+        else if(in[3])
+            pos=2'b11;
+        else 
+            pos=2'b00;
     end
 endmodule
