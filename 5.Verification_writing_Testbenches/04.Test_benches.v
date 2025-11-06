e2ad85217b4fef20f7273b30f473a32f01e0b367
Gokul S <sarangokul86@gmail.com>
2025-11-06 21:33:37 +0530

    Create 04.Test_benches.v

---
+++ 5.Verification_writing_Testbenches/04.Test_benches.v
@@ -0,0 +1,18 @@
+module top_module();
+reg clk,in;
+    reg [2:0]s;
+    wire out;
+    q7 dut(clk,in,s,out);
+    initial begin
+        clk=0;
+        forever #5 clk=~clk;
+    end
+    initial begin
+        in=0;s=3'd2;#10
+        in=0;s=3'd6;#10 
+        in=1;s=3'd2;#10
+        in=0;s=3'd7;#10
+        in=1;s=3'd0;#30
+        in=0;
+    end
+endmodule
